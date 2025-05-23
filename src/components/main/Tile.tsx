5e6a48036e1e234d6898fc60a408b79b065851e6
Vagelis Kosmiadis <vageliskosmiadis@gmail.com>
2025-05-23 15:54:01 +0300

    Added Tile and Tiles components for home page

--- src/components/main/Tile.tsx
+++ src/components/main/Tile.tsx
@@ -1,3 +1,44 @@
-export default function Tile () {
-    return <></>
+import { useCallback, useEffect, useRef, useState } from "react"
+import { Link } from "react-router-dom"
+
+type TileProps = { label: string, to: string, start: number, end: number }
+
+export default function Tile ({ label, to, start, end }: TileProps) {
+
+    const tileRef = useRef<HTMLAnchorElement>(null);
+
+    const [ mousePosition, setMousePosition ] = useState<{ x: null | number, y: null | number }>({ x: 0, y: 0});
+
+    const updateMousePosition = useCallback((e: MouseEvent) => {
+
+        // relative position of mouse when entering tile
+        const bounds = tileRef.current!.getBoundingClientRect();
+        const x = e.clientX - bounds.left;
+        const y = e.clientY - bounds.top;
+        setMousePosition({ x, y });
+    }, []);
+
+    useEffect(() => {
+        console.log(mousePosition);
+    }, [mousePosition])
+
+
+    function handleMouseEnterAnimation () {
+        tileRef.current!.addEventListener('mousemove', updateMousePosition);
+    }
+
+    function handleMouseLeaveAnimation () {
+        tileRef.current!.removeEventListener('mousemove', updateMousePosition);
+    }
+
+    useEffect(() => {
+        tileRef.current!.style.perspective = `${Math.round(mousePosition.x!) / 10 }`
+    }, [mousePosition]);
+
+
+    return <Link ref={tileRef} to={to} viewTransition={true} className={`hover:rotate-${Math.round(mousePosition.x!) / 100 } col-start-${start} col-end-${end} tile tile-hover rounded-xl duration-100`} onMouseLeave={handleMouseLeaveAnimation} onMouseEnter={handleMouseEnterAnimation} >
+        <div className="flex flex-col justify-end p-2 h-full">
+            <span className="font-bold text-2xl">{label}</span>
+        </div>
+    </Link>
 }
--- src/components/main/Tiles.tsx
+++ src/components/main/Tiles.tsx
@@ -1,4 +1,10 @@
+import { APP_DATA } from "@data/data.ts"; 
+import Tile from "./Tile";
+
 export default function Tiles () {
-    return <>
-    </>
+    const tiles = APP_DATA.MainContent.tiles;
+    
+    return <section className="flex flex-col tablet:grid grid-cols-3 gap-4">
+        {tiles.map(tile => <Tile key={tile.label} label={tile.label} to={tile.to} start={tile.col_start} end={tile.col_end} />)}
+    </section>
 }

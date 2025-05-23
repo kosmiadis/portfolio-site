f04c1c3e1f939704736a33d922c6b8024b9ed62f
Vagelis Kosmiadis <vageliskosmiadis@gmail.com>
2025-05-23 14:32:13 +0300

    Added Button UI Component

---
+++ src/components/ui/Button/Button.tsx
@@ -0,0 +1,7 @@
+import type { ReactNode } from "react"
+
+type ButtonProps = { children: ReactNode }
+
+export default function Button ({ children }:  ButtonProps) {
+    return <button>{children}</button>
+}

0a9b3bfda038757c561a08e0b240380184c885c4
Vagelis Kosmiadis <vageliskosmiadis@gmail.com>
2025-05-23 13:11:16 +0300

    Added label tags for tiles

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,7 +1,15 @@
-// type APP_DATA_TYPE = {
-//     navigation: {  }[]
-// }
+type APP_DATA_TYPE = {
+    MainContent: { tiles: { label: string, to?: string }[] }
+}
 
-export const APP_DATA = {
-
+export const APP_DATA: APP_DATA_TYPE = {
+    MainContent: {
+        tiles: [
+            { label: 'Projects', to: '/projects' },
+            { label: 'Skills', to: '/skills'},
+            { label: 'About', to: '/about' },
+            { label: 'CV' },
+            { label: 'Contact', to: '/contact' },
+        ]
+    }
 }

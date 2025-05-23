35152c81cb961d27479044206a543b1a030fa7ce
Vagelis Kosmiadis <vageliskosmiadis@gmail.com>
2025-05-23 18:17:09 +0300

    Removed attributes

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,15 +1,15 @@
 type APP_DATA_TYPE = {
-    MainContent: { tiles: { label: string, to?: string, col_start: number, col_end: number }[] }
+    MainContent: { tiles: { label: string, to?: string }[] }
 }
 
 export const APP_DATA: APP_DATA_TYPE = {
     MainContent: {
         tiles: [
-            { label: 'Projects', to: '/projects', col_start: 1, col_end: 3 },
-            { label: 'Skills', to: '/skills', col_start: 2, col_end: 3},
-            { label: 'About', to: '/about', col_start: 1, col_end: 1},
-            { label: 'CV', col_start: 2, col_end: 2 },
-            { label: 'Contact', to: '/contact', col_start: 3, col_end: 3},
+            { label: 'Projects', to: '/projects'},
+            { label: 'Skills', to: '/skills'},
+            { label: 'About', to: '/about'},
+            { label: 'CV'},
+            { label: 'Contact', to: '/contact'},
         ]
     }
 }

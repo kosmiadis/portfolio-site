b84039c605898a0787c7473b3026644964f4c024
Vagelis Kosmiadis <vageliskosmiadis@gmail.com>
2025-05-23 15:53:05 +0300

    Made changes to header and footer

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -3,8 +3,8 @@ import { Link, useLocation } from "react-router-dom";
 export default function Footer () {
     const { pathname } = useLocation();
 
-    return <footer className="text-foreground-primary font-mono flex-row-between">
-        <Link viewTransition={true} to={pathname === '/' ? '/blog' : '/'} className="hover:underline font-semibold decoration-2 decoration-accent primary-paragraph">{pathname === '/' ? 'Blog' : 'Home'}</Link>
-        <p className="text-foreground-primary primary-paragraph">Built & Designed by <span className="text-foreground-secondary font-semibold">Vagelis Kosmiadis</span></p>
+    return <footer className="p-4 flex flex-col laptop:flex-row text-foreground-primary font-mono flex-row-between secondary-paragraph laptop:primary-paragraph">
+        <Link viewTransition={true} to={pathname === '/' ? '/blog' : '/'} className="hover:underline font-semibold decoration-2 decoration-accent">{pathname === '/' ? 'Blog' : 'Home'}</Link>
+        <p className="text-foreground-primary ">Built & Designed by <span className="text-foreground-secondary font-semibold">Vagelis Kosmiadis</span></p>
     </footer>
 }

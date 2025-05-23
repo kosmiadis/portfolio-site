e24cd4855d52225414b4d30b14df9ad810108990
Vagelis Kosmiadis <vageliskosmiadis@gmail.com>
2025-05-23 12:43:44 +0300

    Create layout folder & Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import Header from "@components/Header/Header";
+import Footer from "@components/layout/Footer/Footer"
+import Header from "@components/layout/Header/Header"
 
 function App() {
-  return <div className="">
-    <Header />
+  return <div className="h-[100vh] bg-main-bg flex flex-col">
+    <div className="max-w-(--max-width) w-full m-auto h-5/6">
+      <Header />
+      {/* <Tiles /> */}
+    </div>
+
+    <Footer />
   </div>
 }
 
---
+++ src/components/layout/Footer/Footer.tsx
@@ -0,0 +1,5 @@
+export default function Footer () {
+    return <footer>
+        <p className="text-foreground-primary font-mono">Built by <span className="text-foreground-secondary font-cal-sans">Vagelis Kosmiadis</span></p>
+    </footer>
+}

6afaccdcfd569a0553e7a6dccad0f9f82515102c
Vagelis Kosmiadis <vageliskosmiadis@gmail.com>
2025-05-23 18:03:21 +0300

    Made changes to footer styling

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer () {
     const { pathname } = useLocation();
 
     return <footer className="p-4 flex flex-col laptop:flex-row text-foreground-primary font-mono flex-row-between secondary-paragraph laptop:primary-paragraph">
-        <Link viewTransition={true} to={pathname === '/' ? '/blog' : '/'} className="hover:underline font-semibold decoration-2 decoration-accent">{pathname === '/' ? 'Blog' : 'Home'}</Link>
-        <p className="text-foreground-primary ">Built & Designed by <span className="text-foreground-secondary font-semibold">Vagelis Kosmiadis</span></p>
+        <Link viewTransition={true} to={pathname === '/' ? '/blog' : '/'} className="text-xl laptop:2xl hover:underline font-semibold decoration-2 decoration-accent">{pathname === '/' ? 'Blog' : 'Home'}</Link>
+        <p className="text-foreground-primary text-center ">Built & Designed by <span className="text-foreground-secondary font-semibold">Vagelis Kosmiadis</span></p>
     </footer>
 }

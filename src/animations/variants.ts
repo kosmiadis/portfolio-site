0b1a911f090fae13c54d159259692820a680c5d5
Vagelis Kosmiadis <vageliskosmiadis@gmail.com>
2025-05-24 15:43:13 +0300

    Created global animations

---
+++ src/animations/variants.ts
@@ -0,0 +1,7 @@
+export const animationVariants =  {
+    fadeIn: { opacity: 1 },
+    fadeOut: { opacity: 0 },
+    jumpIn: { scale: 1, opacity: 1 },
+    jumpOut: { scale: 0, opacity: 0 },
+    transitions: { duration: 1 }
+}

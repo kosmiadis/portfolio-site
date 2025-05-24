f028b4b80b5cf10bb3879086da40735fbd253874
Vagelis Kosmiadis <vageliskosmiadis@gmail.com>
2025-05-24 15:44:07 +0300

    Added Not Found Page

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,13 +4,15 @@ import BlogLayout from "@layouts/BlogLayout";
 import CoreLayout from "@layouts/CoreLayout";
 import { createBrowserRouter } from "react-router-dom";
 import Projects from "@pages/Projects";
+import NotFound from "@pages/NotFound";
+import ErrorLayout from "@layouts/ErrorLayout";
 
 export const Router = createBrowserRouter([
     { path: '/', element: <CoreLayout />, children: [
         { path: '', element: <Main />},
         { path: 'projects', element: <Projects />}
-    ]},
+    ], errorElement: <ErrorLayout><NotFound /></ErrorLayout>},
     { path: '/blog', element: <BlogLayout />, children: [
         { path: '', element: <Blog />}
-    ]}
+    ], errorElement: <ErrorLayout><NotFound /></ErrorLayout>},
 ])

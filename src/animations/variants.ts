97bd0fa74358919d33750fe15b0b65d48b27a3f6
Vagelis Kosmiadis <vageliskosmiadis@gmail.com>
2025-05-25 12:26:49 +0300

    Changed background and hover style and animation of tiles

--- src/animations/variants.ts
+++ src/animations/variants.ts
@@ -1,4 +1,6 @@
 export const animationVariants =  {
+    blur: { filter: "blur(2px)", opacity: 0, y: 50},
+    unblur: { filter: "blur(0px)", opacity: 1, y: 0},
     fadeIn: { opacity: 1 },
     fadeOut: { opacity: 0 },
     jumpIn: { scale: 1, opacity: 1 },
--- src/components/main/Tile.tsx
+++ src/components/main/Tile.tsx
@@ -1,36 +1,14 @@
-import { useCallback, useEffect, useRef, useState } from "react"
+import { useRef } from "react"
 import { Link } from "react-router-dom"
 
-type TileProps = { index:number, label: string, to: string }
+type TileProps = { index: number, label: string, to: string | undefined }
 
-export default function Tile ({ index, label, to}: TileProps) {
+export default function Tile ({ label, to}: TileProps) {
 
     const tileRef = useRef<HTMLAnchorElement>(null);
 
-    // const [ mousePosition, setMousePosition ] = useState<{ x: null | number, y: null | number }>({ x: 0, y: 0});
-
-    // const updateMousePosition = useCallback((e: MouseEvent) => {
-
-    //     // relative position of mouse when entering tile
-    //     const bounds = tileRef.current!.getBoundingClientRect();
-    //     const x = e.clientX - bounds.left;
-    //     const y = e.clientY - bounds.top;
-    //     setMousePosition({ x, y });
-    // }, []);
-
-
-
-    // function handleMouseEnterAnimation () {
-    //     tileRef.current!.addEventListener('mousemove', updateMousePosition);
-    // }
-
-    // function handleMouseLeaveAnimation () {
-    //     tileRef.current!.removeEventListener('mousemove', updateMousePosition);
-    // }
-// onMouseLeave={handleMouseLeaveAnimation} onMouseEnter={handleMouseEnterAnimation}
-
-    return <Link ref={tileRef} to={to} viewTransition={true} className={index === 0 ? 'col-span-2' : ''}>
-        <div className={`flex justify-start squares items-end p-4 tile `}>
+    return <Link ref={tileRef} to={to} viewTransition={true} >
+        <div className="flex justify-start items-end p-4 tile">
             <span className="text-shadow-foreground-secondary text-shadow">{label}</span>
         </div>
     </Link>

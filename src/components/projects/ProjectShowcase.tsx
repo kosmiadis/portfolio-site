575e6e24b8b918bf99289828a5fd1b8e58518e4a
Vagelis Kosmiadis <vageliskosmiadis@gmail.com>
2025-05-26 16:17:21 +0300

    styling changes

--- src/components/projects/ProjectShowcase.tsx
+++ src/components/projects/ProjectShowcase.tsx
@@ -15,7 +15,7 @@ export default function ProjectShowcase (project: ProjectI) {
         <button onClick={handleOpenProject} className="project-showcase overflow-hidden hover:cursor-pointer hover-animation relative">
             <div className="flex flex-col relative">
                 <div className="flex-col-between overflow-hidden duration-200 h-full bg-linear-to-tl from-accent to-transparent relative">
-                    <div className="bg-accent"><img src={project.featuredImg} className="grayscale-75 hover:grayscale-0 z-[1] w-full rounded-t-2xl hover:scale-105 duration-200" /></div>
+                    <div className="bg-accent"><img src={project.featuredImg} className="grayscale-85 hover:grayscale-0 z-[1] w-full rounded-t-2xl hover:scale-105 duration-200" /></div>
                 </div>
                 <h2 className="text-lg p-2 w-full text-left z-20 absolute bottom-0 left-0">{project.title}</h2>
             </div>

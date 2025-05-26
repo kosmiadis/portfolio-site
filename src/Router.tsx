1ea1fdfccf317ba9e1a200738c0d71c17d3c6b53
Vagelis Kosmiadis <vageliskosmiadis@gmail.com>
2025-05-26 22:40:30 +0300

    Registered route paths for about, cv and contact pages

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,11 +6,20 @@ import { createBrowserRouter } from "react-router-dom";
 import Projects from "@pages/Projects";
 import NotFound from "@pages/NotFound";
 import ErrorLayout from "@layouts/ErrorLayout";
+import Skills from "@pages/Skills";
+import About from "@pages/About";
+import CV from "@pages/CV";
+import Contact from "@pages/Contact";
 
 export const Router = createBrowserRouter([
     { path: '/', element: <CoreLayout />, children: [
         { path: '', element: <Main />},
-        { path: 'projects', element: <Projects />}
+        { path: 'projects', element: <Projects />},
+        { path: 'skills', element: <Skills />},
+        { path: 'about', element: <About />},
+        { path: 'cv', element: <CV />},
+        { path: 'contact', element: <Contact />},
+
     ], errorElement: <ErrorLayout><NotFound /></ErrorLayout>},
     { path: '/blog', element: <BlogLayout />, children: [
         { path: '', element: <Blog />}

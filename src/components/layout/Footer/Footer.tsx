3d94efa33341a2f9f0cf91c7ea00dc9b30660a59
Vagelis Kosmiadis <vageliskosmiadis@gmail.com>
2025-05-26 15:06:10 +0300

    Made footer text smaller

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import { Link, useLocation } from "react-router-dom";
 export default function Footer () {
     const { pathname } = useLocation();
 
-    return <footer className="p-4 flex flex-col laptop:flex-row text-foreground-primary font-mono flex-row-between secondary-paragraph laptop:primary-paragraph">
+    return <footer className="p-4 flex flex-col laptop:flex-row text-foreground-primary font-mono flex-row-between text-sm">
         <Link viewTransition={true} to={pathname === '/' ? '/blog' : '/'} className="text-xl laptop:2xl hover:underline font-semibold decoration-2 decoration-accent">{pathname === '/' ? 'Blog' : 'Home'}</Link>
         <p className="text-foreground-primary text-center ">Built & Designed by <span className="text-foreground-secondary font-semibold">Vagelis Kosmiadis</span></p>
     </footer>

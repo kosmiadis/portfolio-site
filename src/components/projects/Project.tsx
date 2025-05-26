5818ea5aaa2be517d1e486ebeef0d87266118ee2
Vagelis Kosmiadis <vageliskosmiadis@gmail.com>
2025-05-26 15:14:54 +0300

    Created projects folder containing Project Showcase Component & Project Modal Component

---
+++ src/components/projects/Project.tsx
@@ -0,0 +1,8 @@
+import type { ProjectI } from "@data/data";
+
+export default function Project ({title, description, featuredImg, github, to, techStack, tags, images}: ProjectI) {
+
+    return <div className="w-full">
+        
+    </div>
+}
---
+++ src/components/projects/ProjectShowcase.tsx
@@ -0,0 +1,24 @@
+import type { ProjectI } from "@data/data";
+import { useProjects } from "@features/project-showcase/store/projects.store";
+import { useEffect } from "react";
+
+export default function ProjectShowcase (project: ProjectI) {
+    const { openProject, setOpenProject } = useProjects();
+
+    function handleOpenProject () {
+        setOpenProject(project)
+    }
+
+    useEffect(() => {}, [openProject]);
+
+    return <div onClick={handleOpenProject}>
+        <button onClick={handleOpenProject} className="project-showcase overflow-hidden hover:cursor-pointer hover-animation relative">
+            <div className="flex flex-col relative">
+                <div className="flex-col-between overflow-hidden duration-200 h-full bg-linear-to-tl from-accent to-transparent relative">
+                    <div className="bg-accent"><img src={project.featuredImg} className="grayscale-75 hover:grayscale-0 z-[1] w-full rounded-t-2xl hover:scale-105 duration-200" /></div>
+                </div>
+                <h2 className="text-lg p-2 w-full text-left z-20 absolute bottom-0 left-0">{project.title}</h2>
+            </div>
+        </button>
+    </div>
+}

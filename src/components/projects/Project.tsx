0090859b4dc3f58a2ed92b4e80112120f6024198
Vagelis Kosmiadis <vageliskosmiadis@gmail.com>
2025-05-26 16:16:52 +0300

    Delete Project Component as it was unnecessary

--- src/components/projects/Project.tsx
+++
@@ -1,8 +0,0 @@
-import type { ProjectI } from "@data/data";
-
-export default function Project ({title, description, featuredImg, github, to, techStack, tags, images}: ProjectI) {
-
-    return <div className="w-full">
-        
-    </div>
-}

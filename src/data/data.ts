704ff631b74ed9239feba20ecd517f962ef32bfc
Vagelis Kosmiadis <vageliskosmiadis@gmail.com>
2025-05-26 16:36:29 +0300

    Fixed images load errors

--- src/data/data.ts
+++ src/data/data.ts
@@ -38,7 +38,7 @@ export const APP_DATA: APP_DATA_TYPE = {
            title: 'Realtime Chat Application', 
            description: 'A modern, full-stack realtime chat application featuring user authentication, live messaging, and online/offline presence detection. Built with Socket.IO for bi-directional communication and Radix UI for a polished interface, the app supports theming (light/dark mode), responsive design, and smooth user feedback through Sonner toasts. TanStack Query handles data fetching and caching, ensuring a seamless and reactive user experience. Users can send messages, see others’ online status in real time, and enjoy a sleek, highly interactive interface.', 
            techStack: ['React.js', 'Node.js', 'Express.js', 'MongoDB', 'Typescript', 'Socket.io'],
-           featuredImg: '/src/assets/chat.png', 
+           featuredImg: '/chat-app/chat_1.png', 
            images: [
                '/chat-app/chat_1.png',
                '/chat-app/chat_2.png',
@@ -58,7 +58,7 @@ export const APP_DATA: APP_DATA_TYPE = {
             title: 'Url Shortener', 
             description: 'A full-stack URL shortener application with real-time analytics and secure user interaction. Each generated URL includes detailed usage statistics visualized through interactive charts. Users can manage their links with visibility toggles, enabling or disabling access. The project emphasizes both frontend interactivity and backend security, incorporating features like JWT authentication and encrypted user data.', 
             techStack: ['React.js', 'Node.js', 'Express.js', 'MongoDB', 'Typescript'],
-            featuredImg: '/src/assets/url.png', 
+            featuredImg:'/url_shortener/url_1.png', 
             images: [
                 '/url_shortener/url_1.png',
                 '/url_shortener/url_2.png',
@@ -82,7 +82,7 @@ export const APP_DATA: APP_DATA_TYPE = {
             title: 'Furniture Restoration Landing Page', 
             description: 'A simple landing page built for a local furniture restoration business. Developed using EJS templating and Express.js, the site includes a functional contact form with real-time email validation and integrated email delivery via Nodemailer. This project emphasizes clean design for small business needs.', 
             techStack: ['Express.js', 'EJS', 'Node.js'],
-            featuredImg: './src/assets/chalk59.png', 
+            featuredImg: '/chalk59/chalk59_1.png',
             images: [
                 '/chalk59/chalk59_1.png',
                 '/chalk59/chalk59_2.png',
@@ -102,7 +102,7 @@ export const APP_DATA: APP_DATA_TYPE = {
             title: 'MyLibrary', 
             description: 'A full-stack digital bookshelf where users can manage the books they own and track those they plan to buy in a dedicated wishlist section. The application features authentication, form validation, and persistent state using cookies. The frontend offers a responsive and animated UI built with NextUI and Framer Motion, while the backend ensures secure data handling and user authentication using JWT and encrypted credentials.', 
             techStack: ['React.js', 'Node.js', 'Express.js', 'MongoDB'],
-            featuredImg: '/src/assets/library.png', 
+            featuredImg: '/my_library/library_1.png', 
             images: [
                 '/my_library/library_1.png',
                 '/my_library/library_2.png',
@@ -123,7 +123,7 @@ export const APP_DATA: APP_DATA_TYPE = {
             title: 'Simple Landing Page', 
             description: 'A pixel-perfect recreation of a pre-built WordPress theme, developed to sharpen my CSS and UI implementation skills. The design was replicated using React and TailwindCSS, with state handled via Zustand.', 
             techStack: ['React.js', 'TailwindCSS', 'Zustand'],
-            featuredImg: '/src/assets/renovation.png', 
+            featuredImg: '/renovation_site/renovation_1.png', 
             images: [
                 '/renovation_site/renovation_1.png',
                 '/renovation_site/renovation_2.png',

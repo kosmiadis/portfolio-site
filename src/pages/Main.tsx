d3b849feb3041b125f044eeccff741ca64d05ec4
Vagelis Kosmiadis <vageliskosmiadis@gmail.com>
2025-05-27 12:31:36 +0300

    Small changes in main.tsx

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -11,7 +11,7 @@ export default function Main () {
     return <Page>
         <div>
             <div className="flex flex-col mb-4">
-                <motion.h1 className="page-hero-text" initial={animationVariants.fadeOut} animate={animationVariants.fadeIn} transition={animationVariants.transitions}>Hello There!</motion.h1>
+                <motion.h1 className="page-hero-text text-5xl" initial={animationVariants.fadeOut} animate={animationVariants.fadeIn} transition={animationVariants.transitions}>Hello There!</motion.h1>
                 <div>
                     {words.map((word, index) => <motion.h1 initial={animationVariants.blur} animate={animationVariants.unblur} transition={{duration: animationVariants.transitions.duration, delay: 0.08 * index}} className="inline text-3xl">{word} </motion.h1>)}
                 </div>

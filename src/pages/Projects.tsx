f58a99738e17b3d61097638d02ac48611c8fe7a6
Vagelis Kosmiadis <vageliskosmiadis@gmail.com>
2025-05-27 20:37:42 +0300

    Small styling changes

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import { motion } from 'motion/react';
 import { animationVariants } from "@animations/variants";
 import ProjectModal from "@features/project-showcase/components/ProjectModal";
+import AnimateChildren from "@components/ui/AnimateChildren/AnimateChildren";
 
 
 export default function Projects () {
@@ -14,9 +15,12 @@ export default function Projects () {
     return <Page>
         <div className="flex flex-col gap-4 relative">
             <h1 className='important-heading text-center'>Here are some of my latest projects!</h1>
-            <div className="w-full grid grid-cols-1 tablet:grid-cols-2 laptop:grid-cols-3 gap-5">
+            <AnimateChildren>
+                {projects.map((project) => <ProjectShowcase key={project.id} {...project} />)}
+            </AnimateChildren>
+            {/* <div className="w-full grid grid-cols-1 tablet:grid-cols-2 laptop:grid-cols-3 gap-5">
                 {projects.map((project, index) => <motion.div initial={animationVariants.blur} animate={animationVariants.unblur} transition={{duration: animationVariants.transitions.duration, delay: 0.05 * index}}  key={project.id}><ProjectShowcase {...project} /></motion.div>)}
-            </div>
+            </div> */}
         </div>
         <ProjectModal />
     </Page>

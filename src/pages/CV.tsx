45c18c28cd7cbb50406bc8b702689ce5eb8a3a75
Vagelis Kosmiadis <vageliskosmiadis@gmail.com>
2025-05-28 22:08:44 +0300

    Finished CV page

--- src/pages/CV.tsx
+++ src/pages/CV.tsx
@@ -1,7 +1,26 @@
+import { animationVariants } from "@animations/variants";
+import Button from "@components/ui/Button/Button"
 import Page from "@components/ui/Page/Page"
+import { motion } from 'motion/react';
 
 export default function CV () {
+
     return <Page>
-        <h2 className="important-heading">CV</h2>
+        <div className="flex-col gap-4 text-center flex-center">
+            
+            <motion.h1 initial={animationVariants.fadeOut}
+                animate={animationVariants.fadeIn}
+                transition={animationVariants.transitions}
+                className="text-4xl font-cal-sans"
+            >Feel free to view or download my CV</motion.h1>
+            
+            <motion.div initial={animationVariants.fadeOut}
+                animate={animationVariants.fadeIn}
+                transition={{ duration: animationVariants.transitions.duration, delay: 0.2}}
+            className="flex gap-2">
+                <a href="VAGELIS_KOSMIADIS_CV.pdf"><Button>View CV</Button></a>
+                <a href="VAGELIS_KOSMIADIS_CV.pdf" download><Button>Download CV</Button></a>
+            </motion.div>
+        </div>
     </Page>
 }

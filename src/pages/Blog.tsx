35327d4264a15ad4b456770feff47c9c3ab31f8c
Vagelis Kosmiadis <vageliskosmiadis@gmail.com>
2025-05-28 22:09:11 +0300

    Added comming soon text on Blog Page

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -2,6 +2,9 @@ import Page from "@components/ui/Page/Page";
 
 export default function Blog () {
     return <Page>
-        <h2 className="important-heading">Blog</h2>
+        <div className="flex flex-col items-center justify-center">
+            <h2 className="important-heading text-3xl">Blog</h2>
+            <p className="font-mono text-3xl">Comming Soon...</p>
+        </div>
     </Page>
 }

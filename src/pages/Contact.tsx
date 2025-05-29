2acde0e77a6d8ff563b4f74acdd8a9fee27c8e04
Vagelis Kosmiadis <vageliskosmiadis@gmail.com>
2025-05-29 12:24:44 +0300

    Install and initialized emailjs instance

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,43 @@
+import emailjs from '@emailjs/browser';
 import Page from "@components/ui/Page/Page"
+import Button from '@components/ui/Button/Button';
 
 export default function Contact () {
+    
+
+
+    function sendEmailResponse (e: React.FormEvent<HTMLFormElement>): void {
+        e.preventDefault();
+
+        const templateParams = {
+            name: 'Vagelis',
+            message: 'Check this out!',
+        };
+    
+        emailjs
+        .send(import.meta.env.EMAILJS_SERVICE_ID, import.meta.env.TEMPLATE_ID, templateParams, {
+          publicKey: 'YOUR_PUBLIC_KEY',
+        })
+        .then(
+          (response) => {
+            console.log('SUCCESS!', response.status, response.text);
+          },
+          (err) => {
+            console.log('FAILED...', err);
+          },
+        );
+    }
+
     return <Page>
         <h2 className="important-heading">Contact</h2>
+
+        <form onSubmit={sendEmailResponse} className='w-full max-w-[400px]'>
+            <div className='bg-accent/40 flex flex-col'>
+                <input type='email' placeholder='Email'/>
+                <input type='text' placeholder='Message' />
+            </div>
+
+            <Button>Send</Button>
+        </form>
     </Page>
 }
